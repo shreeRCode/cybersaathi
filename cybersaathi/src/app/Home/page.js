a78dc4485ba2e87b4205c118f6d49afa1db4ce0c
shreeRCode <mshreeraksha3@gmail.com>
2025-11-20 18:45:25 +0530

    Improved home  page

--- cybersaathi/src/app/Home/page.js
+++ cybersaathi/src/app/Home/page.js
@@ -1,12 +1,18 @@
-import Navbar from '@/components/Navbar/Navbar';
-
-import CyberAwareness from '@/components/background';
+import Navbar from "@/components/Navbar/Navbar";
+import CyberAwareness from "@/components/background";
 
 export default function Homepage() {
   return (
-    <div className='overflow-hidden hide-scrollbar'>
-      <Navbar/>
-      <CyberAwareness/>
+    <div className="overflow-hidden hide-scrollbar bg-gradient-to-b from-blue-50 to-white min-h-screen">
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Main Section */}
+      <div className="pt-24 pb-10 px-6 flex items-center justify-center">
+        <div className="w-full max-w-6xl">
+          <CyberAwareness />
+        </div>
+      </div>
     </div>
   );
 }
